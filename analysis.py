89e77a865d212a425b2ad11222751e9f84af216c
walter-cavinaw <w.cavinaw@gmail.com>
2015-01-17 17:55:30 -0800

    start implementing regression for project

--- analysis.py
+++ analysis.py
@@ -4,45 +4,17 @@ import statsmodels.api as sm
 import datetime
 
 """
-In this optional exercise, you should complete the function called
-predictions(turnstile_weather). This function takes in our pandas
-turnstile weather dataframe, and returns a set of predicted ridership values,
-based on the other information in the dataframe.
-
-In exercise 3.5 we used Gradient Descent in order to compute the coefficients
-theta used for the ridership prediction. Here you should attempt to implement
-another way of computing the coeffcients theta. You may also try using a reference implementation such as:
-http://statsmodels.sourceforge.net/devel/generated/statsmodels.regression.linear_model.OLS.html
-
-One of the advantages of the statsmodels implementation is that it gives you
-easy access to the values of the coefficients theta. This can help you infer relationships
-between variables in the dataset.
-
-You may also experiment with polynomial terms as part of the input variables.
-
-The following links might be useful:
-http://en.wikipedia.org/wiki/Ordinary_least_squares
-http://en.wikipedia.org/w/index.php?title=Linear_least_squares_(mathematics)
-http://en.wikipedia.org/wiki/Polynomial_regression
-
-This is your playground. Go wild!
-
-How does your choice of linear regression compare to linear regression
-with gradient descent computed in Exercise 3.5?
-
-You can look at the information contained in the turnstile_weather dataframe below:
-https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
-
-Note: due to the memory and CPU limitation of our amazon EC2 instance, we will
-give you a random subset (~10%) of the data contained in turnstile_data_master_with_weather.csv
-
-If you receive a "server has encountered an error" message, that means you are hitting
-the 30 second limit that's placed on running your program. See if you can optimize your code so it
-runs faster.
+Here is the analysis for the New York Subway Data with relation to weather.
+It includes the regression model used to predict hourly ridership and the comparison of ridership
+on rainy and non-rainy days.
+This also includes the prediction mechanism I used for Problem Set 3, Question 8.
 """
 
+
 def predictions(weather_turnstile):
-    # weather_turnstile['week_day'] = weather_turnstile['DATEn'].map(lambda x: datetime.datetime.strptime(x,'%Y-%m-%d').day)
+    """Predictions for Problem Set 3, Question 8"""
+    # weather_turnstile['week_day'] = \
+    # weather_turnstile['DATEn'].map(lambda x: datetime.datetime.strptime(x,'%Y-%m-%d').day)
     X = weather_turnstile[['rain', 'Hour', 'meantempi', 'maxtempi', 'meanpressurei']]
     y = weather_turnstile['ENTRIESn_hourly']
     dummy_units = pandas.get_dummies(weather_turnstile['UNIT'], prefix='unit')
@@ -51,18 +23,17 @@ def predictions(weather_turnstile):
 
     est = sm.OLS(y, X)
     est = est.fit()
+    return est.predict(X)
 
-    #model = sm.OLS.from_formula(formula="ENTRIESn_hourly ~ rain + Hour + meantempi + 1", data=features)
+# TODO: implement predictions for new turnstile_weather_v2 and get coefficient, including R^2
 
-    #res = model.fit()
 
-    #print res
-
+def predict(data):
+    X = data[['rain']]
 
 def main():
     print "start"
-    df = pandas.read_csv("improved-dataset/turnstile_data_master_with_weather.csv")
-    predictions(df)
+    df = pandas.read_csv("improved-dataset/turnstile_weather_v2.csv")
     print "done"
 
 if __name__ == "__main__":
